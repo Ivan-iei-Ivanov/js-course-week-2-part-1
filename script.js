81d6251643a6d615a1bc63e5e705aae5b84c4389
Ivan Ivanov <ivan.iei.ivanov@gmail.com>
2023-06-12 16:20:13 +0300

    solution for secondDigit

--- script.js
+++ script.js
@@ -1,18 +1,13 @@
 var firstDigit   = 0;
-var birthYear    = prompt("Моля въведете годината си на раждане:");
-var clientGender = "";
-var clientAge    = 0;
-var randomNumber = 0;
-// if (birthYear < 1965) {
-//     firstDigit = 1;
-// } else if (birthYear > 1965) {
-//     firstDigit = 2;
-// } else if (birthYear = 1965) {
-//     firstDigit = 0;
-// }
-// if (birthYear != 1965){
+var secondDigit  = 0;
+var thirdigit    = 0;
+var forthDigit   = 0;
+var fifthDigit   = 0;
+var sixthDigit   = 0;
 
-// }
+var birthYear    = prompt("Моля въведете годината си на раждане:");
+var clientGender = ""; //male, female, other
+var clientAge    = 0;
 
 
 if (birthYear < 1965) {
@@ -32,4 +27,13 @@ if (firstDigit >= 10) {
   firstDigit = Math.floor(firstDigit / 10);
 }
 
+if (clientGender === "male"){
+    secondDigit = 1;
+} else if (clientGender === "female"){
+    secondDigit = 3;
+} else {
+    do {
+    randomNumberForGender   = Math.floor(Math.random() * 9) + 1;
+    } while (randomNumberForGender != 1 && 3);
+}
 
